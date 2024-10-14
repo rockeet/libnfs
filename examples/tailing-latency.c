b1dea49d2d0e3b7c0827f9ce0645d223b43950ad
leipeng <peng@topling.cn>
2024-10-14 22:48:06 +0800

    examples/tailing-latency.c: support native open for read
    usage: tailing-latency WriterFile NfsReaderFile
    
    If NfsReaderFile starts with "nfs://", using libnfs for read,
    else use native open+read.
    
    WriterFile NfsReaderFile should be a same file, the 2 file's
    container directory maybe mounted as different file system.
    
    As a special case, the 2 file lies in a EOF blocking file system
    for tailing read, which then exported as NFS.
    
    Here is our EOF blocking file system by fuse:
    https://github.com/rockeet/libfuse/commit/passthrough-tailing-read

--- examples/tailing-latency.c
+++ examples/tailing-latency.c
@@ -64,8 +64,8 @@ void print_usage(const char* prog) {
 }
 
 struct PageMem {
-    struct timespec t;
-    char pad[4096 - sizeof(struct timespec)];
+	struct timespec t;
+	char pad[4096 - sizeof(struct timespec)];
 };
 struct PageMem wpage, rpage;
 size_t rwsize = sizeof(struct PageMem);
@@ -81,8 +81,9 @@ void* read_proc(void* unused) {
 		struct timespec t1, t2, t3, t4;
 		clock_gettime(CLOCK_MONOTONIC, &t1);
 		while (1) {
-			ssize_t n = nfs_read(nfs, fr, &rpage, rwsize);
-            t2 = rpage.t;
+			ssize_t n = nfs ? nfs_read(nfs, fr, &rpage, rwsize)
+			                : read((intptr_t)fr, &rpage, rwsize);
+			t2 = rpage.t;
 			clock_gettime(CLOCK_MONOTONIC, &t3);
 			if (n < 0) {
 				fprintf(stderr, "read(%zd) = %zd : %m\n", rwsize, n);
@@ -90,10 +91,10 @@ void* read_proc(void* unused) {
 			}
 			if (n == 0) {
 				Sleep(sleepms);
-    			clock_gettime(CLOCK_MONOTONIC, &t4);
-                sum3 += (t4.tv_sec - t3.tv_sec) * 1e9 + (t4.tv_nsec - t3.tv_nsec);
+				clock_gettime(CLOCK_MONOTONIC, &t4);
+				sum3 += (t4.tv_sec - t3.tv_sec) * 1e9 + (t4.tv_nsec - t3.tv_nsec);
 				continue;
-            }
+			}
 			if (n != rwsize) {
 				fprintf(stderr, "read(%zd) = %zd : %m\n", rwsize, n);
 				exit(1);
@@ -109,7 +110,7 @@ void* read_proc(void* unused) {
 			break;
 		}
 	}
-    return NULL;
+	return NULL;
 }
 
 // Bench mark nfs tailing read latency, WriterFile and NfsReaderFile should be
@@ -131,16 +132,16 @@ void* read_proc(void* unused) {
 //      - ~ 1~9ms without topling passthrough, sleep time is many
 //
 int main(int argc, char *argv[]) {
-	int flags = 0, dsync = 0, fw, ret;
+	int flags, dsync = 0, fw, ret;
 	char *url = NULL, *server = NULL, *export = NULL, *path = NULL, *strp;
-    const char* str_flags = NULL;
-    pthread_t thr;
-    sleepms = atoi(getenv("sleepms")?:"10");
-    rwsize = atoi(getenv("rwsize")?:"4096");
-    if (rwsize < sizeof(struct timespec))
-        rwsize = sizeof(struct timespec);
-    if (rwsize > sizeof(struct PageMem))
-        rwsize = sizeof(struct PageMem);
+	pthread_t thr;
+	sleepms = atoi(getenv("sleepms")?:"10");
+	rwsize = atoi(getenv("rwsize")?:"4096");
+	if (rwsize < sizeof(struct timespec))
+		rwsize = sizeof(struct timespec);
+	if (rwsize > sizeof(struct PageMem))
+		rwsize = sizeof(struct PageMem);
+	printf("rwsize: %zd\n", rwsize);
 
 #ifdef WIN32
 	if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
@@ -158,107 +159,123 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	url = argv[2];
-
-    // for simple, not parse port!
-    // nfs://server/export/dir/filename
-	if (strncmp(url, "nfs://", 6)) {
-		fprintf(stderr, "Invalid URL specified.\n");
-		print_usage(argv[0]);
-		exit(0);
-	}
-	server = strdup(url + 6);
-	if (server == NULL) {
-		fprintf(stderr, "Failed to strdup server string\n");
-		exit(10);
-	}
-	if (server[0] == '/' || server[0] == '\0') {
-		fprintf(stderr, "Invalid server string.\n");
-		free(server);
-		exit(10);
-	}
-	strp = strchr(server, '/');
-	if (strp == NULL) {
-		fprintf(stderr, "Invalid URL specified.\n");
-		print_usage(argv[0]);
-		free(server);
-		exit(0);
-	}
-	export = strdup(strp);
-	if (export == NULL) {
-		fprintf(stderr, "Failed to strdup server string\n");
-		free(server);
-		exit(1);
-	}
-    path = strrchr(export, '/');
-	if (export == NULL) {
-		fprintf(stderr, "Bad path: %s\n", export);
-        free(export);
-		free(server);
-		exit(1);
-	}
-    *path++ = '\0'; // for both export and path
-	*strp = 0; // for server
-    printf("rwsize: %zd\n", rwsize);
-    printf("server: %s\n", server);
-    printf("export: %s\n", export);
-    printf("path  : %s\n", path);
-
-    if (access(argv[1], O_RDONLY) == 0) {
-        fprintf(stderr, "ERROR: file %s exists\n", argv[1]);
-        return 1;
-    }
-
-  #define SetFlags(f) flags = f, str_flags = #f;
-    if (atoi(getenv("O_DSYNC")?:"1")) {
-        SetFlags(O_CREAT|O_WRONLY|O_DSYNC);
-    } else {
-        SetFlags(O_CREAT|O_WRONLY);
-    }
-    dsync = atoi(getenv("dsync")?:"0");
-	fw = open(argv[1], flags, 0777);
-	if (fw < 0) {
-		fprintf(stderr, "open(%s, %s, 0777) = %m\n", argv[1], str_flags);
+	if (access(argv[1], O_RDONLY) == 0) {
+		fprintf(stderr, "ERROR: file %s exists\n", argv[1]);
 		return 1;
 	}
 
-	nfs = nfs_init_context();
-	if (nfs == NULL) {
-		printf("failed to init context\n");
-		goto finished;
+  #define SetFlag(f, Default) flags |= atoi(getenv(#f)?:#Default) ? f : 0
+  #define StrFlag(f) flags & f ? "|" #f : ""
+  #define O_DIRECTW  O_DIRECT
+  #define O_DIRECTR  O_DIRECT
+
+	flags = O_CREAT|O_WRONLY;
+	SetFlag(O_DSYNC,   1);
+	SetFlag(O_DIRECT,  0);
+	SetFlag(O_DIRECTW, 1);
+	dsync = atoi(getenv("dsync")?:"0");
+	printf("write flags: 0%s%s\n", StrFlag(O_DIRECT), StrFlag(O_DSYNC));
+	fw = open(argv[1], flags, 0777);
+	if (fw < 0) {
+		fprintf(stderr, "open(%s, O_CREAT|O_WRONLY%s, 0777) = %m\n",
+			argv[1], StrFlag(O_DSYNC));
+		return 1;
 	}
 
-	ret = nfs_mount(nfs, server, export);
-	if (ret != 0) {
- 		printf("Failed to mount nfs share : %s\n", nfs_get_error(nfs));
-		goto finished;
-	}
+	url = argv[2];
 
-	ret = nfs_open(nfs, path, O_RDONLY, &fr);
-	if (ret != 0) {
-		printf("Failed to nfs_open(%s) %s\n", path, nfs_get_error(nfs));
-		exit(1);
-	}
+	// for simple, not parse port!
+	// nfs://server/export/dir/filename
+	if (strncmp(url, "nfs://", 6) == 0) {
+		server = strdup(url + 6);
+		if (server == NULL) {
+			fprintf(stderr, "Failed to strdup server string\n");
+			exit(10);
+		}
+		if (server[0] == '/' || server[0] == '\0') {
+			fprintf(stderr, "Invalid server string.\n");
+			free(server);
+			exit(10);
+		}
+		strp = strchr(server, '/');
+		if (strp == NULL) {
+			fprintf(stderr, "Invalid URL specified.\n");
+			print_usage(argv[0]);
+			free(server);
+			exit(0);
+		}
+		export = strdup(strp);
+		if (export == NULL) {
+			fprintf(stderr, "Failed to strdup server string\n");
+			free(server);
+			exit(1);
+		}
+		path = strrchr(export, '/');
+		if (export == NULL) {
+			fprintf(stderr, "Bad path: %s\n", export);
+			free(export);
+			free(server);
+			exit(1);
+		}
+		*path++ = '\0'; // for both export and path
+		*strp = 0; // for server
+		printf("server: %s\n", server);
+		printf("export: %s\n", export);
+		printf("path  : %s\n", path);
 
-    if (pthread_create(&thr, NULL, &read_proc, NULL)) {
-        perror("pthread_create");
-        goto close_files;
-    }
+		nfs = nfs_init_context();
+		if (nfs == NULL) {
+			printf("failed to init context\n");
+			goto finished;
+		}
+		ret = nfs_mount(nfs, server, export);
+		if (ret != 0) {
+			printf("Failed to mount nfs share : %s\n", nfs_get_error(nfs));
+			goto finished;
+		}
+		ret = nfs_open(nfs, path, O_RDONLY, &fr);
+		if (ret != 0) {
+			printf("Failed to nfs_open(%s) %s\n", path, nfs_get_error(nfs));
+			exit(1);
+		}
+		printf("libnfs open for read success\n");
+	}
+	else {
+		flags = O_RDONLY;
+		SetFlag(O_RSYNC  , 0); // has no effect on linux
+		SetFlag(O_DIRECT , 0);
+		SetFlag(O_DIRECTR, 1);
+		printf("read  flags: 0%s%s\n", StrFlag(O_DIRECT), StrFlag(O_RSYNC));
+		fr = (struct nfsfh*)(intptr_t)open(url, flags);
+		if ((intptr_t)(fr) < 0) {
+			fprintf(stderr, "open(%s, O_RDONLY%s%s) = %m\n",
+				url, StrFlag(O_RSYNC), StrFlag(O_DIRECT));
+			exit(1);
+		}
+		printf("native open for read success\n");
+	}
+	if (pthread_create(&thr, NULL, &read_proc, NULL)) {
+		perror("pthread_create");
+		goto close_files;
+	}
 	while (1) {
 		clock_gettime(CLOCK_MONOTONIC, &wpage.t);
 		ssize_t len = write(fw, &wpage, rwsize);
-        if (len != rwsize) {
-            perror("write");
-            goto close_files;
-        }
-        if (dsync)
-            fdatasync(fw);
-        Sleep(sleepms);
+		if (len != rwsize) {
+			perror("write");
+			goto close_files;
+		}
+		if (dsync)
+			fdatasync(fw);
+		Sleep(sleepms);
 	}
-    pthread_join(thr, NULL);
+	pthread_join(thr, NULL);
 
 close_files:
-	nfs_close(nfs, fr);
+	if (nfs)
+		nfs_close(nfs, fr);
+	else
+		close((intptr_t)(fr));
 	close(fw);
 
 finished:
